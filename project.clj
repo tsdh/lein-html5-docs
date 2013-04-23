27da3a6d32e710ad4f7edc6d213214a82a62cd11
Tassilo Horn <tsdh@gnu.org>
2013-04-23 14:06:47 +0200

    Improve search & render faster

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-;; IMPORTANT: When bumping the version number here, be sure to bump it also in
-;; src/leiningen/html5_docs.clj!
-(defproject lein-html5-docs "2.0.0"
+;; TODO: When bumping the version number here, be sure to bump it also in
+;; src/leiningen/html5_docs/core.clj (var lein-html5-docs-version).
+(defproject lein-html5-docs "2.0.1"
   :description "A HTML5 API docs generator plugin for Leiningen.
 Versions below 1.2.0 are for Leiningen 1, starting with 1.2.0 Leiningen 2 is
 required."
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [;;[org.clojure/clojure "1.5.1"]
                  [hiccup "1.0.3"]]
   :jar-exclusions [#"(?:^|/).git/"]
   :warn-on-reflection true
--- src/leiningen/html5_docs.clj
+++ src/leiningen/html5_docs.clj
@@ -1,10 +1,11 @@
 (ns leiningen.html5-docs
   "Generate HTML5 API docs."
-  (:use [leiningen.core.eval :only [eval-in-project]]))
+  (:use [leiningen.core.eval :only [eval-in-project]])
+  (:use [leiningen.html5-docs.core :only [lein-html5-docs-version]]))
 
 (defn html5-docs [project]
   (eval-in-project
    (update-in project [:dependencies]
-              conj ['lein-html5-docs "1.2.3"])
+              conj ['lein-html5-docs lein-html5-docs-version])
    `(leiningen.html5-docs.core/html5-docs '~project)
    '(require 'leiningen.html5-docs.core)))
